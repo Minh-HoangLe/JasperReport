b89be377020fcbdbc667d18d942877ef045d05bc
Minh Hoang <minh.hoang@dounets.com>
2022-05-07 16:49:45 +0700

    stable

--- src/main/java/com/clv/jasperReport/controllers/EmployeeController.java
+++ src/main/java/com/clv/jasperReport/controllers/EmployeeController.java
@@ -2,21 +2,27 @@ package com.clv.jasperReport.controllers;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+
 import com.clv.jasperReport.exception.EmployeeNotFoundException;
 import com.clv.jasperReport.models.Employee;
 import com.clv.jasperReport.services.EmployeeService;
+import com.clv.jasperReport.services.JasperReportCompile;
+import com.clv.jasperReport.services.JasperReportCompile.ExportType;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -27,62 +33,63 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.engine.util.JRSaver;
 
 @RestController
 @RequestMapping("/employee/")
 @Slf4j
 public class EmployeeController {
-    
+
     @Autowired
     private EmployeeService service;
+
+    @Autowired
+    private JasperReportCompile compile;
 
     @GetMapping("getAll")
     public ResponseEntity<List<Employee>> searchAllEmployee() {
         return ResponseEntity.ok(service.getAllEmployees());
     }
 
-    @GetMapping("/pdf")
-    public ResponseEntity<byte[]> exportToPDF() throws FileNotFoundException, JRException {
-        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(service.getAllEmployees(),false);
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("title","Test Title");
-        JasperReport compileReport = JasperCompileManager.compileReport(new FileInputStream("src/main/resources/reports/employeeReport.jrxml"));
-        // JRSaver.saveObject(compileReport, "src/main/resources/reports/employeeReport.jasper");
-        JasperPrint jasperPrint = JasperFillManager.fillReport(compileReport, parameters, dataSource);
-        byte[] data = JasperExportManager.exportReportToPdf(jasperPrint);
+    // @GetMapping("/pdf")
+    // public ResponseEntity<byte[]> exportToPDF() throws FileNotFoundException,
+    // JRException {
+    // JRBeanCollectionDataSource dataSource = new
+    // JRBeanCollectionDataSource(service.getAllEmployees(), false);
+    // Map<String, Object> parameters = new HashMap<>();
+    // parameters.put("title", "Test Title");
+    // JasperReport compileReport = JasperCompileManager
+    // .compileReport(new
+    // FileInputStream("src/main/resources/reports/employeeReport.jrxml"));
+    // JasperPrint jasperPrint = JasperFillManager.fillReport(compileReport,
+    // parameters, dataSource);
+    // byte[] data = JasperExportManager.exportReportToPdf(jasperPrint);
 
-		log.info("exportToPDF : {}",data);
-
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-Disposition", "inline; filename=employeesReport.pdf");
-
-		return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(data);
-
-    }
-
-    // @GetMapping("/html")
-    // public ResponseEntity<byte[]> exportToHTML() throws FileNotFoundException, JRException {
-    //     JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(service.getAllEmployees(),false);
-    //     Map<String, Object> parameters = new HashMap<>();
-    //     parameters.put("title","Test Title");
-    //     JasperReport compileReport = JasperCompileManager.compileReport(new FileInputStream("src/main/resources/reports/employeeReport.jrxml"));
-    //     // JRSaver.saveObject(compileReport, "src/main/resources/reports/employeeReport.jasper");
-    //     JasperPrint jasperPrint = JasperFillManager.fillReport(compileReport, parameters, dataSource);
-    //     byte[] data = JasperExportManager.exportReportToHtmlFile(jasperPrint,);
-
-	// 	log.info("exportToPDF : {}",data);
-
-	// 	HttpHeaders headers = new HttpHeaders();
-	// 	headers.add("Content-Disposition", "inline; filename=employeesReport.pdf");
-
-	// 	return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF).body(data);
+    // return ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF).body(data);
 
     // }
 
+    @GetMapping("/export")
+    public void exportToXLS(HttpServletResponse response, @RequestParam String fileName, @RequestParam ExportType type)
+            throws JRException, IOException, SQLException {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("title", "Test Title");
+        switch (type) {
+            case EXCEL:
+                compile.exportToExcel(response, fileName, parameters);
+                break;
+            case PDF:
+                compile.exportToPDF(response, fileName, parameters);
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
     @ExceptionHandler(EmployeeNotFoundException.class)
-	public ResponseEntity<String> handleEmployeeNotFoundException() {
-		return ResponseEntity.ok("Employee Not Found");
-	}
+    public ResponseEntity<String> handleEmployeeNotFoundException() {
+        return ResponseEntity.ok("Employee Not Found");
+    }
 
 }
---
+++ src/main/java/com/clv/jasperReport/services/JasperReportCompile.java
@@ -0,0 +1,109 @@
+package com.clv.jasperReport.services;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+import java.sql.SQLException;
+import java.util.Map;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.ResourceUtils;
+
+import lombok.extern.slf4j.Slf4j;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
+import net.sf.jasperreports.export.SimplePdfReportConfiguration;
+import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
+
+@Service
+public class JasperReportCompile {
+
+    public enum ExportType {
+        PDF,
+        EXCEL
+    }
+
+    private static final String REPORT_FOLDER = "src/main/resources/reports/";
+
+    @Autowired
+    private DataSource dataSource;
+
+    // public byte[] exportToPDF(String fileName, Map<String, Object> parameters)
+    // throws FileNotFoundException, JRException, SQLException {
+
+    // JasperReport compileReport = JasperCompileManager
+    // .compileReport(new FileInputStream(REPORT_FOLDER + fileName + ".jrxml"));
+    // JasperPrint jasperPrint = JasperFillManager.fillReport(compileReport,
+    // parameters, dataSource.getConnection());
+    // return JasperExportManager.exportReportToPdf(jasperPrint);
+    // }
+
+    public void exportToExcel(HttpServletResponse response, String fileName, Map<String, Object> parameters)
+            throws JRException, IOException, SQLException {
+        JasperReport compileReport = JasperCompileManager
+                .compileReport(new FileInputStream(REPORT_FOLDER + fileName + ".jrxml"));
+        JasperPrint jasperPrint = JasperFillManager.fillReport(compileReport, parameters, dataSource.getConnection());
+
+        ServletOutputStream streamDeSaida = response.getOutputStream();
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-disposition", "attachments; filename=\"employeeReport.xlsx\"");
+
+        JRXlsxExporter exporter = new JRXlsxExporter();
+        SimpleXlsxReportConfiguration reportConfigXLS = new SimpleXlsxReportConfiguration();
+        reportConfigXLS.setSheetNames(new String[] { "Employee Data" });
+        exporter.setConfiguration(reportConfigXLS);
+        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(streamDeSaida));
+        exporter.exportReport();
+    }
+
+    public void exportToPDF(HttpServletResponse response, String fileName, Map<String, Object> parameters)
+            throws JRException, SQLException, IOException {
+                JasperReport compileReport = JasperCompileManager
+                .compileReport(new FileInputStream(REPORT_FOLDER + fileName + ".jrxml"));
+        JasperPrint jasperPrint = JasperFillManager.fillReport(compileReport, parameters, dataSource.getConnection());
+
+        ServletOutputStream streamDeSaida = response.getOutputStream();
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-disposition", "attachments; filename=\"employeeReport.pdf\"");
+
+        JRPdfExporter exporter = new JRPdfExporter();
+
+        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(streamDeSaida));
+
+        SimplePdfReportConfiguration reportConfig = new SimplePdfReportConfiguration();
+        reportConfig.setSizePageToContent(true);
+        reportConfig.setForceLineBreakPolicy(false);
+
+        SimplePdfExporterConfiguration exportConfig = new SimplePdfExporterConfiguration();
+        exportConfig.setMetadataAuthor("CLV");
+        exportConfig.setEncrypted(true);
+        exportConfig.setAllowedPermissionsHint("PRINTING");
+
+        exporter.setConfiguration(reportConfig);
+        exporter.setConfiguration(exportConfig);
+     
+        exporter.exportReport();
+    }
+}
